ab922d2422d2eec4d1ee58b0ee62acd26b39a074
zanniboni <viny.zanchagas@gmail.com>
2023-04-03 20:14:33 -0300

    refactor codebase

---
+++ src/components/Home/HomeScreen.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { View } from 'react-native';
+import Header from '../Layout/Header';
+import BtnNavigation from './Navigation/BtnNavigation';
+
+function HomeScreen({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center' }}>
+        <Header title={"GreenyTime"} />
+        <BtnNavigation title={'Registrar Salário'} route={'RegisterSalary'} navigation={navigation}/>
+        <BtnNavigation title={'Registrar horas'} route={'WorkHours'} navigation={navigation}/>
+      </View>
+    );
+}
+
+export default HomeScreen;
